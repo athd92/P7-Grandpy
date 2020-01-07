742e4e81a27725afda602c97cb636f36f3f6eff6
athd92 <antoinecours@gmx.fr>
2020-01-07 20:48:14 +0100

    bug fix

--- controler/googleMaps.py
+++ controler/googleMaps.py
@@ -16,28 +16,23 @@ class GoogleMaps:
         key = os.environ['API_KEY_MAPS']
         gmaps = googlemaps.Client(key)
         # Geocoding an address
-
-        res = gmaps.geocode(self.query)
-        if res == []:
+        print('TEST')
+        geocode_result = gmaps.geocode(self.query)
+        if geocode_result == []:
             self.query = [self.query, 'france']
-            res = gmaps.geocode(self.query)
+            geocode_result = gmaps.geocode(self.query)
+            print(geocode_result)
         try:
-            streetNumber = res[0]['address_components'][0]['short_name']
-            streetName = res[0]['address_components'][1]['long_name']
-            streetCity = res[0]['address_components'][2]['long_name']
-            streetComplement = res[0]['address_components'][3]['long_name']
-            streetCityName = res[0]['address_components'][4]['long_name']
-            streetCitySecondName = res[0]['address_components'][5]['long_name']
-            streetCityCountry = res[0]['address_components'][6]['long_name']
+            streetNumber = geocode_result[0]['address_components'][0]['short_name']
+            streetName = geocode_result[0]['address_components'][1]['long_name']
+            streetCity = geocode_result[0]['address_components'][2]['long_name']
+            streetComplement = geocode_result[0]['address_components'][3]['long_name']
 
             address = streetNumber + '\
                 ' + streetName + ' \
                 ' + streetCity + ' \
-                ' + streetComplement + ' \
-                ' + streetCityName + ' \
-                ' + streetCitySecondName + ' \
-                ' + streetCityCountry
+                ' + streetComplement
 
-            return res, address
+            return geocode_result, address
         except ValueError:
-            return res
+            return geocode_result
