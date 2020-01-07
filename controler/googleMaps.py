6cc0cafdf74699e0b3952122e5adb1ce3f4fb593
athd92 <antoinecours@gmx.fr>
2020-01-07 21:06:43 +0100

    update

--- controler/googleMaps.py
+++ controler/googleMaps.py
@@ -16,18 +16,16 @@ class GoogleMaps:
         key = os.environ['API_KEY_MAPS']
         gmaps = googlemaps.Client(key)
         # Geocoding an address
-        print('TEST')
         geocode_result = gmaps.geocode(self.query)
         if geocode_result == []:
             self.query = [self.query, 'france']
             geocode_result = gmaps.geocode(self.query)
-            print(geocode_result)
         try:
             streetNumber = geocode_result[0]['address_components'][0]['short_name']
             streetName = geocode_result[0]['address_components'][1]['long_name']
             streetCity = geocode_result[0]['address_components'][2]['long_name']
             streetComplement = geocode_result[0]['address_components'][3]['long_name']
-
+            
             address = streetNumber + '\
                 ' + streetName + ' \
                 ' + streetCity + ' \
